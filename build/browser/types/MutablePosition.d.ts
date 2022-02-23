08b0b7984e32b08b9627b7e377cfdd0ca2efd94a
David Geo Holmes <geometryzen@gmail.com>
2022-02-22 22:27:00 -0500

    stemcstudio-build

--- build/browser/types/MutablePosition.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class MutablePosition {
-    line: number;
-    column: number;
-    constructor(line: number, column: number);
-    offset(rows: number, cols: number): void;
-    toString(): string;
-}
